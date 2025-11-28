4c7dc78d3e030f0a2b92c6302c96621ce8a93479
carolswee <carolsweerenz@gmail.com>
2025-11-28 14:58:44 +0300

    function to remove duplicates from a list

--- func.py
+++ func.py
@@ -8,3 +8,10 @@ def func(names):
 list1=['carol','ann','jay','rose']
 func(list1)
 
+#function to remove duplicates from a list
+def remove_duplicates(random_list):
+    new_list=list(set(random_list))
+    return new_list
+
+cars=['vitz','demio','vitz','subaru']
+print(remove_duplicates(cars))
